5bdc41349c462aff30f166ccaa172475a92b66e0
Pablo <60163907+pablovargas234@users.noreply.github.com>
2020-03-11 10:20:34 -0600

    Add files via upload

---
+++ arboldeasteriscos.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+int A;
+print("cuantos renglones quieres que tenga el arbol:");
+scanf("%d" ,&A);
+
+    for(int i=1; I<=A; i++)
+{
+    for(int/j=1; j<=A-i; j++)
+        printf (" ");
+    for(int j=1; j<=2*i-1; j++)
+    printf("*");
+    printf("\n");
+
+}
+      }
